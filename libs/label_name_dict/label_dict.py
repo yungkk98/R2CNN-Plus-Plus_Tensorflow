9c891737b81b6cdd57d75b8fd3aa1b1b3455d504
yungkk98 <yungkk87@gmail.com>
2020-02-14 02:00:24 +0900

    Update label_dict.py

--- libs/label_name_dict/label_dict.py
+++ libs/label_name_dict/label_dict.py
@@ -4,7 +4,7 @@ from __future__ import division, print_function, absolute_import
 from libs.configs import cfgs
 
 if cfgs.DATASET_NAME == 'ship':
-    NAME_LABEL_MAP = {'container': 0, 'oil': 1, 'carrier': 2, 'etc': 3}
+    NAME_LABEL_MAP = {'background': 0, 'container': 1, 'oil': 2, 'carrier': 3, 'etc': 4}
 elif cfgs.DATASET_NAME == 'aeroplane':
     NAME_LABEL_MAP = {
         'back_ground': 0,
